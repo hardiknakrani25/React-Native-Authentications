0b8c2bbce40a2db71dd78ea9a1aa087287987ad4
Hardik <hdnakarani@gmail.com>
2019-04-06 18:03:57 +0530

    create user in for new user

--- OTP_Firebse/functions/index.js
+++ OTP_Firebse/functions/index.js
@@ -1,8 +1,11 @@
-const functions = require('firebase-functions');
+const admin = require("firebase-admin");
+const functions = require("firebase-functions");
+const createUser = require("./createUser");
+const serviceAccount = require("./service_account.json");
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://otp-react-native-63424.firebaseio.com"
+});
+
+exports.createUser = functions.https.onRequest(createUser);
